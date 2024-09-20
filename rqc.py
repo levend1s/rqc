7f13c11c158d775ecdab39c1e9bb3c0c4c5af04f
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2024-09-20 23:35:50 +1000

    add min length filter, add find reads common to input bams

--- rqc.py
+++ rqc.py
@@ -8,6 +8,9 @@ parser.add_argument('function')
 parser.add_argument('inputs', nargs='+')
 parser.add_argument('-q', '--min_phred', type=int, default=0)
 parser.add_argument('-m', '--min_mapq', type=int, default=0)
+parser.add_argument('-l', '--min_read_length', type=int, default=0)
+parser.add_argument('--check_duplicate_reads', type=bool, default=False)
+parser.add_argument('-v', '--verbose', type=bool, default=False)
 parser.add_argument('-o', '--outfile', type=str)
 
 args = parser.parse_args()
@@ -15,8 +18,10 @@ args = parser.parse_args()
 INPUT = args.inputs
 MIN_PHRED = args.min_phred
 MIN_MAPQ = args.min_mapq
+MIN_READ_LENGTH = args.min_read_length
 FUNCTION = args.function
 OUTFILE = args.outfile
+CHECK_DUPLICATE_READS = args.check_duplicate_reads
 
 # this calculates the NX for a reverse sorted list of read lengths
 # You might use this to calculate the N50 or N90, to find the read 
@@ -55,9 +60,6 @@ def print_tlen_distribution(tlens):
         n50 = getNX(sorted_tlens, 0.5)
         n90 = getNX(sorted_tlens, 0.9)
 
-
-        a = [100, 70, 60, 50, 50, 40, 30]
-
         summary = {
             "sample": key,
             "count": len(cur_tlens),
@@ -90,7 +92,6 @@ def print_tlen_distribution(tlens):
         print()
 
 
-
 def plot_qscore_hists(qscores):
     for key in qscores.keys():
         bins = numpy.arange(numpy.floor(qscores[key].min()), numpy.ceil(qscores[key].max()))
@@ -109,12 +110,39 @@ def plot_mapq_hists(mapqs):
     plt.hist(mapqs.values(), histtype="bar", label=mapqs.keys(), density=True)
     plt.legend(loc="upper right")
 
+def find_multi_reference_alignments(d_reads):
+    keys = list(d_reads.keys())
+    d_intersects = {}
+
+    for i in list(range(len(keys))):
+        for j in list(range(i+1, len(keys))):
+            intersect = [x for x in d_reads[keys[i]] if x in d_reads[keys[j]]]
+
+            for r in intersect:
+                if r in d_intersects:
+                    if keys[j] not in d_intersects[r]:
+                        d_intersects[r].append(keys[j])
+                else:
+                    d_intersects[r] = [keys[i], keys[j]]
+
+    # print summaries
+    print("FOUND {} READS COMMON TO SAMPLES:".format(len(d_intersects.keys())))
+
+    for r in d_intersects.keys():
+        print("{}\t".format(r), end="")
+
+        for s in d_intersects[r]:
+            print("{}\t".format(s), end="")
+        print()
+
+    # extra newline :_)
+    print()
 
 if FUNCTION == "plot":
     d_phred = {}
     d_mapq = {}
     d_tlen = {}
-
+    d_read_ids = {}
 
     for i in range(0, len(INPUT), 2):
         label = INPUT[i]
@@ -123,30 +151,33 @@ if FUNCTION == "plot":
         phred_scores = []
         mapq_scores = []
         template_lengths = []
+        read_ids = []
 
 
         samfile = pysam.AlignmentFile(filename, 'rb')
         iter = samfile.fetch()
 
         for x in iter:
-            # and not x.is_secondary ???
-            if (x.mapping_quality >= MIN_MAPQ and x.get_tag('qs') >= MIN_PHRED):
+            if (not x.is_secondary and 
+                x.mapping_quality >= MIN_MAPQ and 
+                x.get_tag('qs') >= MIN_PHRED and 
+                len(x.query_sequence) >= MIN_READ_LENGTH):
+                
                 phred_scores.append(round(x.get_tag('qs'), 2))
                 mapq_scores.append(x.mapping_quality)
                 # our dorado modbam files have TLEN as 0, why? Not all entries have an MN:i tag, why?
-                if not x.is_secondary:
-                    template_lengths.append(len(x.query_sequence))
-
-                # if len(x.query_sequence) > 100000:
-                #     print("caught a big one: {}".format(len(x.query_sequence)))
-                #     print(x)
+                template_lengths.append(len(x.query_sequence))
+                read_ids.append(x.query_name)
 
         d_phred[label] = numpy.array(phred_scores)
         d_mapq[label] = mapq_scores
         d_tlen[label] = template_lengths
+        d_read_ids[label] = read_ids
 
         samfile.close()
 
+    if CHECK_DUPLICATE_READS:
+        find_multi_reference_alignments(d_read_ids)
     print_tlen_distribution(d_tlen)
 
     plot_qscore_hists(d_phred)
