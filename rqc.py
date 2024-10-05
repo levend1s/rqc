854fe389fbfd0ec8906f05f562c47df683cc1ee5
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2024-10-05 19:44:56 +1000

    add base_coverage function to calculate total of each base read given a gff TYPE

--- rqc.py
+++ rqc.py
@@ -330,7 +330,92 @@ import gffpandas.gffpandas as gffpandas
 import sys
 import scipy
 
-if FUNCTION == "coverage":
+if FUNCTION == "base_coverage":
+    # load annotation file
+    feature_id = INPUT[0]
+    print("summarising base coverage for {}...".format(feature_id))
+
+    annotation_file = gffpandas.read_gff3(ANNOTATION_FILE)
+
+    if feature_id == "chromosome":
+        matches = pandas.DataFrame(columns = ["seq_id", "start", "end"])
+        i = 0
+        for line in annotation_file.header.splitlines():
+            if "sequence-region" in line:
+                s = line.split(" ")
+
+                matches.loc[i] = [s[1]] + [int(s[2])] + [int(s[3])]
+                i += 1
+        num_matches = len(matches)
+
+
+    else:
+        matches = annotation_file.filter_feature_of_type([feature_id])
+        if len(matches.df) == 0:
+            print("WARNING: no matches of type {}".format(feature_id))
+            matches = annotation_file.get_feature_by_attribute("ID", [feature_id])
+
+            if len(matches.df) > 1:
+                print("ERROR: multiple entries for {} found in gff file. exiting...".format(feature_id))
+                sys.exit()
+
+        num_matches = len(matches.df)
+        matches = matches.df
+        print(matches)
+
+    print("FOUND {} MATCHES FOR {}".format(num_matches, feature_id))
+
+
+    for i in range(1, len(INPUT), 2):
+        label = INPUT[i]
+        filename = INPUT[i+1]
+        samfile = pysam.AlignmentFile(filename, 'rb')
+
+        output = pandas.DataFrame(columns = ["seq_id", "start", "end", "count_a", "count_t", "count_c", "count_g"])
+
+        for index, row in matches.iterrows():
+            # DEBUGGING
+            print(index)
+            # if i == num_matches:
+            #     break
+
+            # TODO: if two genes are close to each other, then this doesn't discern for only reads mapped to our gene of interest
+            # so we can end up with weird lumps in the 5' end
+            a, c, t, g = samfile.count_coverage(
+                contig=row['seq_id'], 
+                start=row['start'], 
+                stop=row['end'],
+                quality_threshold=0
+            )
+            for column in samfile.pileup(
+                contig=row['seq_id'], 
+                start=row['start'], 
+                stop=row['end'], 
+                min_mapping_quality=MIN_MAPQ,
+                truncate = True
+            ):
+                print(column)
+
+            sum_a = sum(a)
+            sum_t = sum(t)
+            sum_c = sum(c)
+            sum_g = sum(g)
+
+            output.loc[index] = [row['seq_id']] + [row['start']] + [row['end']] + [sum_a] + [sum_t] + [sum_c] + [sum_g]
+
+
+        print(output)
+        print("total {} {} {} {}".format(
+            output['count_a'].sum(),
+            output['count_t'].sum(),
+            output['count_c'].sum(),
+            output['count_g'].sum()
+        ))
+        
+            
+
+
+if FUNCTION == "plot_coverage":
     # load annotation file
     feature_id = INPUT[0]
     print("plotting gene coverage for {}...".format(feature_id))
@@ -525,8 +610,8 @@ if FUNCTION == "find_dmr":
     df['same_treatment_average'] = same_treatment_columns.mean(numeric_only=True, axis=1)
     df['diff_treatment_average'] = diff_treatment_columns.mean(numeric_only=True, axis=1)
 
-    SAME_TREATMENT_THRESHOLD = 0
-    DIFF_TREATMENT_THRESHOLD = 1
+    SAME_TREATMENT_THRESHOLD = 0.1
+    DIFF_TREATMENT_THRESHOLD = 10
 
     df['differentially_expressed'] = False
     df.loc[(df['same_treatment_average'] <= SAME_TREATMENT_THRESHOLD) & (df['diff_treatment_average'] >= DIFF_TREATMENT_THRESHOLD), 'differentially_expressed'] = True
